e24747908ab5695508bbf64875172b53dc3dab02
Christopher Hernandez <cerberean661@gmail.com>
2022-03-03 18:50:32 -0800

    syntax notes

---
+++ practice js/syntax.js
@@ -0,0 +1,24 @@
+// var box = "honda civic"
+// // var box = 3
+
+// console.log(box)
+
+
+// var a = 3
+// var b = 6
+
+// console.log(a + b)
+// console.log(a * b)
+// console.log(b - a)
+// console.log(b / a)
+
+
+
+var studentPassed = false
+console.log(studentPassed)
+
+if (studentPassed) {
+    console.log("Student has passed!")
+} else {
+    console.log("Student did not pass.")
+}
