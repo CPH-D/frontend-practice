75705618830ed02970472f9b075272112dba87ef
Christopher Hernandez <cerberean661@gmail.com>
2022-03-04 21:54:45 -0800

    array, objects notes added

--- practice js/syntax.js
+++ practice js/syntax.js
@@ -12,13 +12,40 @@
 // console.log(b - a)
 // console.log(b / a)
 
+// ================================================================================================
 
+// var studentPassed = false
+// console.log(studentPassed)
 
-var studentPassed = false
-console.log(studentPassed)
+// if (studentPassed) {
+//     console.log("Student has passed!")
+// } else {
+//     console.log("Student did not pass.")
+// }
 
-if (studentPassed) {
-    console.log("Student has passed!")
-} else {
-    console.log("Student did not pass.")
+// ================================================================================================
+// array
+
+// var userData = ["joe", 123, "chris", 4567, 8, "james", 90]
+// console.log(userData)
+
+// ================================================================================================
+// objects
+
+var person = {
+    name: "Chris",
+    age: 37,
+    kids: false,
+    study: ["AS", "Web-Dev"],
+    myName: function () {
+        console.log("my name is Chris")
+    }
 }
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.kids)
+console.log(person.study)
+console.log(person.myName())
+
+// ================================================================================================
