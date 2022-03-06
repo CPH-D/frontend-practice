501d8c277c61ab56c39e21c6be65e8d6bf106034
Christopher Hernandez <cerberean661@gmail.com>
2022-03-05 18:23:58 -0800

    numbers js file created, notes added

---
+++ practice js/numbers.js
@@ -0,0 +1,10 @@
+var exEquation = (((((4 + 2) * 2) * 8) / 2) % 2)
+console.log(exEquation)
+
+var apples = 4
+var oranges = 8
+var fruits = oranges + apples
+
+console.log(fruits)
+
+// ================================================================================================
