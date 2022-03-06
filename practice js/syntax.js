f5157f9dd1672d8e2e73d12207404d4d414d4abb
Christopher Hernandez <cerberean661@gmail.com>
2022-03-05 17:23:59 -0800

    operations notes added for syntax

--- practice js/syntax.js
+++ practice js/syntax.js
@@ -32,20 +32,48 @@
 // ================================================================================================
 // objects
 
-var person = {
-    name: "Chris",
-    age: 37,
-    kids: false,
-    study: ["AS", "Web-Dev"],
-    myName: function () {
-        console.log("my name is Chris")
-    }
-}
+// var person = {
+//     name: "Chris",
+//     age: 37,
+//     kids: false,
+//     study: ["AS", "Web-Dev"],
+//     myName: function () {
+//         console.log("My name is Chris")
+//     }
+// }
 
-console.log(person.name)
-console.log(person.age)
-console.log(person.kids)
-console.log(person.study)
-console.log(person.myName())
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.kids)
+// console.log(person.study)
+// console.log(person.myName())
 
 // ================================================================================================
+
+// operations
+
+// var a = "5"
+// var b = 5
+
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a != b)
+// console.log(a !== b)
+// console.log(a >= b)
+
+// if (a == b) {
+//     console.log("It is equal.")
+// } else {
+//     console.log("It is NOT equal.")
+// }
+
+var user = {
+    name: "Chris",
+    age: 37
+}
+
+if (user.name == "Chris" && user.age == 36) {
+    console.log("User is valid.")
+} else {
+    console.log("Incorrect user information.")
+}
