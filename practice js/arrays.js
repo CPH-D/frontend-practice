c205edc22bbb3248ef0d1ae4c449d51a928bef73
Christopher Hernandez <cerberean661@gmail.com>
2022-03-06 18:35:31 -0800

    array.js added, with notes

---
+++ practice js/arrays.js
@@ -0,0 +1,11 @@
+var users = ["Adrian", "Brian", "Charlie", "Dianne", "Emily"]
+
+// console.log(users[2])
+
+var userData = ["Adrian", 2, { user: "Brian" }, ["array1", "array2"], "Dianne", "Emily"]
+
+// console.log(userData)
+console.log(userData[2].user)
+
+// ================================================================================================
+
