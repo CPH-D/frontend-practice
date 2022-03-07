f0751bd71c4d289f9604ac983dfa45d1191e3d24
Christopher Hernandez <cerberean661@gmail.com>
2022-03-06 17:49:43 -0800

    strings notes added

---
+++ practice js/strings.js
@@ -0,0 +1,21 @@
+var aName = "James"
+
+console.log("my name is " + aName)
+console.log(aName.length)
+console.log(aName.toUpperCase())
+
+if (aName.length <= 10) {
+    console.log("Name is short")
+} else {
+    console.log("Name is long")
+}
+
+// ================================================================================================
+
+var myFirstName = "Christopher"
+var myLastName = "Hernandez"
+
+console.log(myFirstName)
+console.log(myFirstName.toLowerCase())
+console.log(myFirstName.toUpperCase())
+console.log(myFirstName + " " + myLastName)
