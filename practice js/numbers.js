8648ee975452ea565c189459d394eaaf8e787a9a
Christopher Hernandez <cerberean661@gmail.com>
2022-03-06 17:28:34 -0800

    numbers js notes added

--- practice js/numbers.js
+++ practice js/numbers.js
@@ -8,3 +8,17 @@ var fruits = oranges + apples
 console.log(fruits)
 
 // ================================================================================================
+
+var m = 1.23456789
+var n = 9.87654321
+
+console.log(m.toFixed(2))
+console.log(n.toFixed(2))
+
+// ================================================================================================
+
+var a = 5 * 20
+var apples = (((16 - 2) * 2) / 4)
+
+console.log(apples)
+console.log(a * apples)
