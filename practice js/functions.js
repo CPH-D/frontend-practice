21a41b329860c74ac7fb62abab2daa416d3ff271
Christopher Hernandez <cerberean661@gmail.com>
2022-03-16 13:57:00 -0700

    var & functions notes added

--- practice js/functions.js
+++ practice js/functions.js
@@ -1,5 +1,14 @@
+lastName("Hernandez")
+
+// variable coding must input first before running it
 var usersName = function (name) {
-    console.log('This user\'s name is ' + name)
+    console.log('This user\'s first name is ' + name)
 }
 
 usersName("Chris")
+
+// functions automatically goes to memory before anything else on the page runs
+function lastName(last) {
+    console.log('This user\'s last name is ' + last)
+}
+
