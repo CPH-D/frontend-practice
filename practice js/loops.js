855d094889a795b2e6e353ddb7c616bc8f5edcf5
Christopher Hernandez <cerberean661@gmail.com>
2022-03-22 14:27:07 -0700

    loops excercises done

--- practice js/loops.js
+++ practice js/loops.js
@@ -67,14 +67,58 @@ for (m of mentors) {
 }
 
 console.log("============================================================")
-console.log("=======================EXCERCISE============================")
+console.log("=======================EXCERCISES===========================")
 console.log("============================================================")
 
 // print your name 3 times in a loop
+var myName = ["Chris", "Chris", "Chris"]
+var n = 0
 
+for (n = 0; n < 3; n++) {
+    if (n == 2) {
+        console.log(myName[n])
+    } else {
+        console.log(myName[n])
+    }
+}
 
 // create an array named brands with 4 of your favorite name brands then choose whatever loop you like to print all 4 brands
 
+var company = ["Nintendo", "Sega", "Sony", "Microsoft"]
+
+var c = 0
+
+for (c of company) {
+    console.log(c)
+}
 
 // create an array with objects inside that contain information of each brand but it must have name, product, and price inside.  then print a sentence that says "(brandname)'s product cost (price)"
 
+var brand = [
+    {
+        name: "Nintendo",
+        product: "Smash Bros",
+        price: 40
+    },
+    {
+        name: "Mojang",
+        product: "Minecraft",
+        price: 28
+    },
+    {
+        name: "SquareEnix",
+        product: "Final Fantasy XIV",
+        price: 15
+    },
+    {
+        name: "Rockstar",
+        product: "Grand Theft Auto V",
+        price: 60
+    }
+]
+
+var b = 0
+
+for (b of brand) {
+    console.log(b.name + "'s product cost is $" + b.price + ".")
+}
